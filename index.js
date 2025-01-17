17334c31b2e86a53c7f721e55d22af40da3b0ad5
TUHINDAS9670 <drup9672@gmail.com>
2025-01-17 21:09:18 +0530

    backend part complete

--- index.js
+++ index.js
@@ -27,4 +27,4 @@ const PORT =  process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server running in ${process.env.DEV_MODE} mode on port ${process.env.PORT} successfuly`.bgWhite.green);
-});
+}); 
